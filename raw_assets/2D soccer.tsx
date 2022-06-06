6a545e995cf5e1c747fd1e22c252fdd922254614
DavideVitali <davide.bass@gmail.com>
2022-06-06 19:53:17 +0200

    commit

--- raw_assets/2D soccer.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="2D soccer" tilewidth="12" tileheight="12" tilecount="153" columns="17">
- <image source="PlatformerPack_1/grass-tilemap-1.png" width="208" height="112"/>
-</tileset>
