37acd8ee210d800b04b4a756827f3f027468e91c
Davide Vitali <davide.bass@gmail.com>
2023-03-13 13:52:33 +0100

    temp resulting js

--- dist/js/types.js
+++
@@ -1,7 +0,0 @@
-export var FormationType;
-(function (FormationType) {
-    FormationType[FormationType["OFFENSE"] = 0] = "OFFENSE";
-    FormationType[FormationType["DEFENSE"] = 1] = "DEFENSE";
-})(FormationType || (FormationType = {}));
-;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidHlwZXMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvdHlwZXMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQ0EsTUFBTSxDQUFOLElBQVksYUFBa0M7QUFBOUMsV0FBWSxhQUFhO0lBQUcsdURBQU8sQ0FBQTtJQUFFLHVEQUFPLENBQUE7QUFBQyxDQUFDLEVBQWxDLGFBQWEsS0FBYixhQUFhLFFBQXFCO0FBQUEsQ0FBQyJ9
